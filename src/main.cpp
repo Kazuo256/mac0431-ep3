bc1b3925bb6d224350cc492662cdc31a89765129
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-12-07 00:38:25 -0200

    Enbonitando

--- src/main.cpp
+++ src/main.cpp
@@ -10,49 +10,69 @@
 
 // CGM headers
 #include "Comm.h"
-#include "GeneralUtilities.h"
+#include "BasicCommObject.h"
 
 // Local headers
-#include "utils.h"
-#include "PairCommObject.h"
+// TODO?
 
 using std::cout;
 using std::endl;
 using std::ios_base;
+using std::ifstream;
 using std::ofstream;
 
+//==== Process management related ====//
 // Number of processes
 static int p;
 // ID of this process
 static int id;
 
-// Dimensions of the matrix chain
-static vector<unsigned> dims;
-
+//==== Communication related ====//
+// Communication object
+static Comm                     *comm = NULL;
 // Zero
 static BasicCommObject<double>  zero(0.0);
 
-// Load matrix dimensions from input file
-static void load_input (const string& intutfile);
+//==== Algorithm related ====//
+// Dimensions of the matrix chain
+static vector<unsigned>           dims;
+// Matrix of costs
+static vector< vector<unsigned> > costs;
 
+// Get data from CommObject
 static double get_data (CommObject* obj) {
   return dynamic_cast<BasicCommObject<double>*>(obj)->getData();
 }
 
+// Set data to CommObject
 static void set_data (CommObject* obj, double value) {
   dynamic_cast<BasicCommObject<double>*>(obj)->setData(value);
 }
 
-int main (int argc, char **argv) {
-  Comm *comm = Comm::getComm (&argc, &argv, NULL);
-  p = comm->getNumberOfProcessors ();
-  id = comm->getMyId ();
+// Load matrix dimensions from input file
+static void load_input (const string& inputfile) {
+  ifstream input(inputfile.c_str());
+  while (input.good()) {
+    double dim;
+    input >> dim;
+    dims.push_back(dim);
+  }
+  input.close();
+}
 
+int main (int argc, char **argv) {
+  // Startup values
+  comm  = Comm::getComm (&argc, &argv, NULL);
+  p     = comm->getNumberOfProcessors ();
+  id    = comm->getMyId ();
+
+  // Arg check
   if (argc != 2) {
     cout << "[Proccess " << p << "] Aborting due to invalid arguments." << endl;
     return EXIT_FAILURE;
   }
 
+  // Arg handle
   load_input(argv[1]);
 
   CommObjectList  data(&zero);
@@ -63,7 +83,7 @@ int main (int argc, char **argv) {
   set_data(data[0], 1.0);
 
   for (int i = 0; i < p-id; ++i) {
-    //comm->synchronize();
+    // contas...
     if (id != Comm::PROC_ZERO)
       comm->send(id-1, data, 0);
     if (i+1 < p-id) {
@@ -84,9 +104,5 @@ int main (int argc, char **argv) {
 
   comm->dispose ();
 
-}
-
-void load_input (const string& inputfile) {
-  
 }
 
